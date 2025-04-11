53416e582316fe43936ddda7e55ad5501c7ecc41
William <54956314+Happy-design@users.noreply.github.com>
2025-04-11 14:36:17 +0800

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,33 +1,42 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from flask import Flask, request
+import telegram
+import os
 
-BOT_TOKEN = "7432110325:AAH6NTVYz3QJUZaCD5IOqh8hoxQS4hZDJPM"
+app = Flask(__name__)
+
+BOT_TOKEN = '7432110325:AAH6NTVYz3QJUZaCD5IOqh8hoxQS4hZDJPM'
+bot = telegram.Bot(token=BOT_TOKEN)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Welcome to the food expenditure prediction. Please enter your income:')
+YOUR_URL = 'https://ai-final-z5c0.onrender.com'
 
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    if text.lower() == 'exit':
-        await update.message.reply_text('Goodbye!')
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.set_webhook(f'{YOUR_URL}/webhook')
+    if s:
+        return "Webhook setup ok"
     else:
-        try:
-            income = float(text)
-            prediction = (income * 0.4851) + 147.4
-            await update.message.reply_text(f'Your predicted food expenditure is {prediction:.2f}')
-        except ValueError:
-            await update.message.reply_text('Invalid input. Please enter a valid number.')
+        return "Webhook setup failed"
 
-def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler('start', start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    print("Bot is running...")
-    app.run_polling()
+    if update.message:
+        text = update.message.text
 
-if __name__ == '__main__':
-    main()
+        if text.lower() == 'exit':
+            update.message.reply_text('Goodbye!')
+        else:
+            try:
+                income = float(text)
+                prediction = (income * 0.4851) + 147.4
+                update.message.reply_text(f'Your predicted food expenditure is {prediction:.2f}')
+            except ValueError:
+                update.message.reply_text('Invalid input. Please enter a valid number.')
+    return 'ok'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
