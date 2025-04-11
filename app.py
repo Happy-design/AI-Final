ea26bdf80717e42e64306944e4596c3ecf39ea83
William <54956314+Happy-design@users.noreply.github.com>
2025-04-11 14:44:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import datetime
 import google.generativeai as genai
 import os
 import wikipedia
+import telegram
 
 api = os.getenv("makersuite")
 model = genai.GenerativeModel("gemini-1.5-flash")
@@ -12,6 +13,11 @@ genai.configure(api_key=api)
 app = Flask(__name__)
 
 flag = 1
+
+BOT_TOKEN = "7432110325:AAH6NTVYz3QJUZaCD5IOqh8hoxQS4hZDJPM"
+bot = telegram.Bot(token=BOT_TOKEN)
+
+YOUR_URL = "https://ai-final-z5c0.onrender.com"
 
 @app.route("/",methods=["POST","GET"])
 def index():
@@ -98,6 +104,32 @@ def deleteLog():
     c.close()
     conn.close
     return(render_template("deleteLog.html"))
+
+@app.route('/set_webhook', methods=["GET", "POST"])
+def set_webhook():
+    s = bot.set_webhook(f'{YOUR_URL}/webhook')
+    if s:
+        return "Webhook setup OK"
+    else:
+        return "Webhook setup Failed"
+
+@app.route('/webhook', methods=["POST"])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    if update.message:
+        text = update.message.text
+
+        if text.lower() == 'exit':
+            update.message.reply_text('Goodbye!')
+        else:
+            try:
+                income = float(text)
+                prediction = (income * 0.4851) + 147.4
+                update.message.reply_text(f'Your predicted food expenditure is {prediction:.2f}')
+            except ValueError:
+                update.message.reply_text('Invalid input. Please enter a valid number.')
+    return 'ok'
 
 if __name__ == "__main__":
     app.run()
